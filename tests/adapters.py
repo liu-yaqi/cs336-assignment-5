4ba80f30930bb86b53b8049b85b65f4f4cb03e6e
yaqi <liuyq_15@163.com>
2026-04-10 18:24:28 +0800

    adapters

--- tests/adapters.py
+++ tests/adapters.py
@@ -11,8 +11,8 @@ from transformers import PreTrainedTokenizerBase
 from cs336_alignment import sft as sft_module
 from cs336_alignment import utils as sft_utils_module
 from cs336_alignment import grpo_helper as grpo_module
-# from cs336_alignment import dpo as dpo_module
-# from cs336_alignment import data as data_module
+from cs336_alignment.safety_alignment import dpo_helper as dpo_module
+from cs336_alignment.safety_alignment import instruct_data as data_module
 
 
 def run_tokenize_prompt_and_output(
@@ -328,153 +328,152 @@ The below adapters are used in the optional
 RLHF / safety part of the Alignment assignment.
 """
 
+def get_packed_sft_dataset(
+    tokenizer: PreTrainedTokenizerBase,
+    dataset_path: str | os.PathLike,
+    seq_length: int,
+    shuffle: bool,
+) -> Dataset:
+    """
+    Given a tokenizer and a path to a dataset with instruction-tuning examples,
+    construct a PyTorch Dataset for language modeling. The examples should be
+    packed, i.e., all sequences in the dataset are of a constant length (`seq_length`).
 
-# def get_packed_sft_dataset(
-#     tokenizer: PreTrainedTokenizerBase,
-#     dataset_path: str | os.PathLike,
-#     seq_length: int,
-#     shuffle: bool,
-# ) -> Dataset:
-#     """
-#     Given a tokenizer and a path to a dataset with instruction-tuning examples,
-#     construct a PyTorch Dataset for language modeling. The examples should be
-#     packed, i.e., all sequences in the dataset are of a constant length (`seq_length`).
+    Args:
+        tokenizer: transformers.PreTrainedTokenizerBase
+            Transformers tokenizer to use in tokenizing and encoding text.
+        dataset_path: str
+            Path to file with instruction-tuning examples.
+        seq_length: int
+            Number of tokens to include in each example.
+        shuffle: bool
+            If true, shuffle the documents before packing them into examples.
 
-#     Args:
-#         tokenizer: transformers.PreTrainedTokenizerBase
-#             Transformers tokenizer to use in tokenizing and encoding text.
-#         dataset_path: str
-#             Path to file with instruction-tuning examples.
-#         seq_length: int
-#             Number of tokens to include in each example.
-#         shuffle: bool
-#             If true, shuffle the documents before packing them into examples.
-
-#     Returns:
-#         PyTorch Dataset for language modeling. Each example in this dataset is a dictionary of
-#         with keys "input_ids" and "labels" (both tensors of shape (seq_length, )).
-#         "input_ids" contains the token IDs for the language modeling inputs, and "labels" contains
-#         the token IDs for the language modeling labels.
-#     """
-#     return data_module.get_packed_sft_dataset(
-#         tokenizer=tokenizer,
-#         dataset_path=dataset_path,
-#         seq_length=seq_length,
-#         shuffle=shuffle,
-#     )
-
-
-# def run_iterate_batches(
-#     dataset: Dataset,
-#     batch_size: int,
-#     shuffle: bool,
-# ):
-#     """
-#     Given a PyTorch Dataset, return an iterable over batches of size `batch_size`.
-#     Iterating through the returned iterable should constitute one epoch over the Dataset.
-
-#     Args:
-#         dataset: Dataset
-#             Dataset to emit batches from.
-#         batch_size: int
-#             Number of examples to include per batch.
-#         shuffle: bool
-#             If true, shuffle examples before batching them.
-
-#     Returns:
-#         Iterable over batches, where each batch has size `batch_size`.
-#     """
-#     return data_module.iterate_batches(
-#         dataset=dataset,
-#         batch_size=batch_size,
-#         shuffle=shuffle,
-#     )
+    Returns:
+        PyTorch Dataset for language modeling. Each example in this dataset is a dictionary of
+        with keys "input_ids" and "labels" (both tensors of shape (seq_length, )).
+        "input_ids" contains the token IDs for the language modeling inputs, and "labels" contains
+        the token IDs for the language modeling labels.
+    """
+    return data_module.get_packed_sft_dataset(
+        tokenizer=tokenizer,
+        dataset_path=dataset_path,
+        seq_length=seq_length,
+        shuffle=shuffle,
+    )
 
 
-# def run_parse_mmlu_response(
-#     mmlu_example: dict[str, Any],
-#     model_output: str,
-# ) -> str | None:
-#     """
-#     Given an MMLU example and a model output, parse the model output into a
-#     predicted option letter (i.e., 'A', 'B', 'C', or 'D'). If the model output
-#     cannot be parsed into a prediction option letter, return None.
+def run_iterate_batches(
+    dataset: Dataset,
+    batch_size: int,
+    shuffle: bool,
+):
+    """
+    Given a PyTorch Dataset, return an iterable over batches of size `batch_size`.
+    Iterating through the returned iterable should constitute one epoch over the Dataset.
 
-#     mmlu_example: dict[str, Any]
-#         Dictionary with an MMLU example. Contains the following keys:
-#         - "subject": str with the subject of the question.
-#         - "question": str with the text of the question.
-#         - "options": list[str] with the four answer options (in order).
-#                      The first option refers to letter "A", the second to "B", etc.
-#         - "answer": str with the option of the correct answer (e.g., "A")
-#     model_output: str
-#         str with the model's output to the MMLU example.
+    Args:
+        dataset: Dataset
+            Dataset to emit batches from.
+        batch_size: int
+            Number of examples to include per batch.
+        shuffle: bool
+            If true, shuffle examples before batching them.
 
-#     Returns:
-#         str (one of "A", "B", "C", or "D") if the model output can be parsed into a prediction,
-#         else None.
-#     """
-#     return data_module.parse_mmlu_response(
-#         mmlu_example=mmlu_example,
-#         model_output=model_output,
-#     )
+    Returns:
+        Iterable over batches, where each batch has size `batch_size`.
+    """
+    return data_module.iterate_batches(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+    )
 
 
-# def run_parse_gsm8k_response(
-#     model_output: str,
-# ) -> str | None:
-#     """
-#     Given a GSM8K model output, parse the model output into a predicted numeric answer by
-#     taking the last number that occurs in the output.
+def run_parse_mmlu_response(
+    mmlu_example: dict[str, Any],
+    model_output: str,
+) -> str | None:
+    """
+    Given an MMLU example and a model output, parse the model output into a
+    predicted option letter (i.e., 'A', 'B', 'C', or 'D'). If the model output
+    cannot be parsed into a prediction option letter, return None.
 
-#     model_output: str
-#         str with the model's output to a GSM8K example.
+    mmlu_example: dict[str, Any]
+        Dictionary with an MMLU example. Contains the following keys:
+        - "subject": str with the subject of the question.
+        - "question": str with the text of the question.
+        - "options": list[str] with the four answer options (in order).
+                     The first option refers to letter "A", the second to "B", etc.
+        - "answer": str with the option of the correct answer (e.g., "A")
+    model_output: str
+        str with the model's output to the MMLU example.
 
-#     Returns:
-#         str with the predicted numeric answer if the model output can be parsed into a prediction,
-#         else None.
-#     """
-#     return data_module.parse_gsm8k_response(model_output=model_output)
+    Returns:
+        str (one of "A", "B", "C", or "D") if the model output can be parsed into a prediction,
+        else None.
+    """
+    return data_module.parse_mmlu_response(
+        mmlu_example=mmlu_example,
+        model_output=model_output,
+    )
 
 
-# def run_compute_per_instance_dpo_loss(
-#     lm: torch.nn.Module,
-#     lm_ref: torch.nn.Module,
-#     tokenizer: PreTrainedTokenizerBase,
-#     beta: float,
-#     prompt: str,
-#     response_chosen: str,
-#     response_rejected: str,
-# ) -> torch.Tensor:
-#     """
-#     Given two language models (`lm`, and the "reference model" `lm_ref`),
-#     their tokenizer, the DPO beta hyperparameter, a prompt and a pair
-#     of responses to the prompt, computes the value of the DPO loss for this example.
+def run_parse_gsm8k_response(
+    model_output: str,
+) -> str | None:
+    """
+    Given a GSM8K model output, parse the model output into a predicted numeric answer by
+    taking the last number that occurs in the output.
 
-#     lm: torch.nn.Module
-#         Language model being trained.
-#     lm_ref: torch.nn.Module
-#         Reference language model.
-#     tokenizer: PreTrainedTokenizerBase
-#         Tokenizer for both language models.
-#     beta: float
-#         DPO beta hyperparameter.
-#     prompt: str
-#         Prompt for this instance of preference pair.
-#     response_chosen: str
-#         Preferred response to the prompt.
-#     response_rejected: str
-#         Rejected response to the prompt.
+    model_output: str
+        str with the model's output to a GSM8K example.
 
-#     Returns:
-#         torch.Tensor with the DPO loss for this example.
-#     """
-#     return dpo_module.compute_per_instance_dpo_loss(
-#         lm=lm,
-#         lm_ref=lm_ref,
-#         tokenizer=tokenizer,
-#         beta=beta,
-#         prompt=prompt,
-#         response_chosen=response_chosen,
-#         response_rejected=response_rejected,
-#     )
+    Returns:
+        str with the predicted numeric answer if the model output can be parsed into a prediction,
+        else None.
+    """
+    return data_module.parse_gsm8k_response(model_output=model_output)
+
+
+def run_compute_per_instance_dpo_loss(
+    lm: torch.nn.Module,
+    lm_ref: torch.nn.Module,
+    tokenizer: PreTrainedTokenizerBase,
+    beta: float,
+    prompt: str,
+    response_chosen: str,
+    response_rejected: str,
+) -> torch.Tensor:
+    """
+    Given two language models (`lm`, and the "reference model" `lm_ref`),
+    their tokenizer, the DPO beta hyperparameter, a prompt and a pair
+    of responses to the prompt, computes the value of the DPO loss for this example.
+
+    lm: torch.nn.Module
+        Language model being trained.
+    lm_ref: torch.nn.Module
+        Reference language model.
+    tokenizer: PreTrainedTokenizerBase
+        Tokenizer for both language models.
+    beta: float
+        DPO beta hyperparameter.
+    prompt: str
+        Prompt for this instance of preference pair.
+    response_chosen: str
+        Preferred response to the prompt.
+    response_rejected: str
+        Rejected response to the prompt.
+
+    Returns:
+        torch.Tensor with the DPO loss for this example.
+    """
+    return dpo_module.compute_per_instance_dpo_loss(
+        lm=lm,
+        lm_ref=lm_ref,
+        tokenizer=tokenizer,
+        beta=beta,
+        prompt=prompt,
+        response_chosen=response_chosen,
+        response_rejected=response_rejected,
+    )
